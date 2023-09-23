062afa73985ea05eeed5fda3899bef296519d108
Saraiv <mainsaraiva@gmail.com>
2023-09-23 14:22:08 +0100

    About Us done

--- frontend/website-commercial/src/components/AboutUs.jsx
+++ frontend/website-commercial/src/components/AboutUs.jsx
@@ -5,19 +5,21 @@ import { Waves } from './helpers/Lottie'
 const AboutUs = () =>{
     return(
         <div className='h-full flex align-middle justify-start items-center bg-light-white'>
-            <div className='w-2/3 m-auto text-background-main'>
-                <h1 className='text-bold text-9xl py-32'>About Us</h1>
-                <p className='text-4xl'>
+            <div className='w-2/3 lg:text-center m-auto text-background-main'>
+                <h1 className='text-bold text-9xl xl:text-8xl py-32'>About Us</h1>
+                <p className='text-4xl xl:text-3xl'>
                     Sense is a freelance that came up with an idea to help deaf people so that they can feel through vibrations and sensations the rhythm and harmony of any music, style and preference.
                 </p>
-                <Player
-                    ref = {this}
-                    autoplay = {true}
-                    loop = {true}
-                    src = {JSON.stringify(Waves)}
-                    style={{ height: '400px', width: '400px' }}
-                    
-                />
+                <div className='w-full h-fit'>
+                    <Player
+                        ref = {this}
+                        autoplay = {true}
+                        loop = {true}
+                        src = {JSON.stringify(Waves)}
+                        style={{ height: '400px', width: '400px' }}
+                        
+                    />
+                </div>
             </div>
         </div>
     )
