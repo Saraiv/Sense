6af39cac0dc3a455b6026e0b085e5bf2bcb95e5a
Saraiv <mainsaraiva@gmail.com>
2023-09-23 22:24:15 +0100

    Pricing done without carousel

--- frontend/website-commercial/src/components/Pricing.jsx
+++ frontend/website-commercial/src/components/Pricing.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const Pricing = () =>{
+    const windowSize = useRef([window.innerWidth, window.innerHeight])
+
     return(
-        <div className='h-full flex align-middle justify-start items-center bg-light-white'>
+        <div className='h-full lg:h-fit flex align-middle justify-start items-center bg-light-white'>
             <div className='w-2/3 m-auto text-background-main'>
                 <h1 className='py-32 text-bold text-9xl xl:text-8xl lg:text-center'>Pricing</h1>
-                <div className='flex w-full'>
-                    <div className='bg-background-main bg-opacity-80 rounded-3xl w-1/3 h-80 text-light-gray m-auto mr-5 relative'>
+                <div className='flex w-full lg:block'>
+                    <div className='bg-background-main bg-opacity-80 rounded-3xl w-1/3 h-80 text-light-gray m-auto mr-5 relative lg:w-1/2 lg:text-center lg:m-auto lg:mb-4'>
                         <h1 className='p-8 xl:p-6 text-4xl xl:text-3xl'>59,99€</h1>
                         <p className='p-8 xl:p-6'>
                             The chapest package that includes only the device and 1 year warrenty.
@@ -15,7 +17,7 @@ const Pricing = () =>{
                             <a className='font-bold text-lg xl:text-base' href="_blank">More Info Here</a>
                         </div>
                     </div>
-                    <div className='bg-background-main bg-opacity-80 rounded-3xl w-1/3 h-80 text-light-gray m-auto mx-5 relative'>
+                    <div className='bg-background-main bg-opacity-80 rounded-3xl w-1/3 h-80 text-light-gray m-auto mx-5 relative lg:w-1/2 lg:text-center lg:m-auto lg:mb-4'>
                         <h1 className='p-8 xl:p-6 text-4xl xl:text-3xl'>89,99€</h1>
                         <p className='p-8 xl:p-6'>
                             The premium package that includes three devicess and 2 year warrenty.
@@ -24,7 +26,7 @@ const Pricing = () =>{
                             <a className='font-bold text-lg xl:text-base' href="_blank">More Info Here</a>
                         </div>
                     </div>
-                    <div className='bg-background-main bg-opacity-80 rounded-3xl w-1/3 h-80 text-light-gray m-auto ml-5 relative'>
+                    <div className='bg-background-main bg-opacity-80 rounded-3xl w-1/3 h-80 text-light-gray m-auto ml-5 relative lg:w-1/2 lg:text-center lg:m-auto lg:mb-4'>
                         <h1 className='p-8 xl:p-6 text-4xl xl:text-3xl'>119,99€</h1>
                         <p className='p-8 xl:p-6'>
                             The premium package that includes five devices and 3 year warrenty.
--- frontend/website-commercial/src/components/helpers/Images.jsx
+++ frontend/website-commercial/src/components/helpers/Images.jsx
@@ -2,10 +2,12 @@ import Account from '../../recourses/Account.svg'
 import Logo from '../../recourses/Logo.svg'
 import LogoTransparent from '../../recourses/LogoTransparent.svg'
 import LogoDark from '../../recourses/LogoTransparentDark.svg'
+import SoundVectors from '../../recourses/SoundVectors.svg'
 
 export {
     Account,
     Logo,
     LogoTransparent,
-    LogoDark
+    LogoDark,
+    SoundVectors
 }
