29a5300720342be2118bf1fb024e6ee50b6cd7cc
Saraiv <mainsaraiva@gmail.com>
2023-09-27 15:49:09 +0100

    Initial backend commit

--- frontend/website-commercial/src/components/helpers/Lottie.jsx
+++ frontend/website-commercial/src/components/helpers/Lottie.jsx
@@ -1,11 +1,7 @@
 import SplashScreen from '../../lottie/SplashScreen.json'
 import Waves from '../../lottie/Waves.json'
-import HamburgerLight from '../../lottie/HamburgerLight.json'
-import HamburgerDark from '../../lottie/HamburgerDark.json'
 
 export {
     SplashScreen,
-    Waves,
-    HamburgerLight,
-    HamburgerDark
+    Waves
 }
